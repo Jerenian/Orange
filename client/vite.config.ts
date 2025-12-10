15ff97566d8c2a3b884c4b9e9c35f03ea3609345
Jerenian <>
2025-12-10 10:33:33 +0700

    change env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  allowedHosts: ['exactingly-oriented-eel.cloudpub.ru']
+  allowedHosts: ['https://client-4673mo1i7-jerenians-projects.vercel.app']
 }
 })
